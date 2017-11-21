638fd290a20e2528c6a31a99f5440b0862be91ec
Anthony Greenheck <aemgreenheck@gmail.com>
2017-11-21 08:36:52 -0500

    [added] input field

--- src/public/app/Main.jsx
+++ src/public/app/Main.jsx
@@ -3,12 +3,32 @@ import React from 'react';
 class Main extends React.Component {
   constructor(props) {
     super();
-    this.state = {};
+    this.state = {
+      symbolSearch: ''
+    };
+    this.handleInputSymbol = this.handleInputSymbol.bind(this);
+    this.handleSymbolSubmit = this.handleSymbolSubmit.bind(this);
+  }
+  handleInputSymbol(e) {
+    this.setState({
+      symbolSearch: e.target.value
+    });
+  }
+  handleSymbolSubmit() {
+    console.log('submitting symbol!');
   }
   render() {
     return (
       <div className="main-wrapper">
         <h1>SEC</h1>
+        <div>
+          <input
+            placeholder="search"
+            value={this.state.symbolSearch}
+            onChange={this.handleInputSymbol}
+          />
+          <button onClick={this.handleSymbolSubmit}>submit</button>
+        </div>
       </div>
     );
   }
