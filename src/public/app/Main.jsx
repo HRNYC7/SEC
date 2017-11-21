a99a52ee274b9ddef69a5496c64aedec90754fbb
Anthony Greenheck <aemgreenheck@gmail.com>
2017-11-21 11:43:20 -0500

    [added] and styled Search component

--- src/public/app/Main.jsx
+++ src/public/app/Main.jsx
@@ -1,34 +1,18 @@
 import React from 'react';
+import Search from './Search.jsx';
 
 class Main extends React.Component {
-  constructor(props) {
+  constructor() {
     super();
-    this.state = {
-      symbolSearch: ''
-    };
-    this.handleInputSymbol = this.handleInputSymbol.bind(this);
-    this.handleSymbolSubmit = this.handleSymbolSubmit.bind(this);
   }
-  handleInputSymbol(e) {
-    this.setState({
-      symbolSearch: e.target.value
-    });
-  }
-  handleSymbolSubmit() {
-    console.log('submitting symbol!');
+  handleSymbolSubmit(symbol) {
+    console.log(`submitting ${symbol} symbol!`);
   }
   render() {
     return (
-      <div className="main-wrapper">
-        <h1>SEC</h1>
-        <div>
-          <input
-            placeholder="search"
-            value={this.state.symbolSearch}
-            onChange={this.handleInputSymbol}
-          />
-          <button onClick={this.handleSymbolSubmit}>submit</button>
-        </div>
+      <div className="main-container flex-centered">
+        <span className="main-title">SEC</span>
+        <Search handleSymbolSubmit={this.handleSymbolSubmit.bind(this)} />
       </div>
     );
   }
