80e630da03a3286fe219239407f07a525221798b
Anthony Greenheck <aemgreenheck@gmail.com>
2017-11-21 20:07:24 -0500

    [added] diff component and reorganized front end

---
+++ src/public/app/Diff.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Diff extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="diff-container">
+        <span>this is the diff</span>
+      </div>
+    );
+  }
+}
+
+export default Diff;
