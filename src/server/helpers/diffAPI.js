87a875758e3f3d6ec61a1d62eede8733c8c3bbd8
Kendrick Schuettler <keschuettler@gmail.com>
2017-12-06 20:59:56 -0500

    work on jsdom

--- src/server/helpers/diffAPI.js
+++ src/server/helpers/diffAPI.js
@@ -1,15 +1,48 @@
 // Add helper functions related to the Diff API here.
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+const jsdom = require("jsdom");
 
-const filterDomForQtrlyFilingDocumentsURL = function(htmlObj){
-  htmlObj.$('tr').filter(function(x) { 
-    return $(this)
-           .children()
-           .eq(0)[0]
-           .innerText == '10-Q'
-  })
-  .map(function(x) {
-    var a = [];
-    a.push($(this).children().eq(1).children().eq(0)[0].href)
-    return a
-  })
-} 
+const apiHelpers = {
+  filterDomFor10QURL: function(htmlObj){
+    return fetch(`https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=${cik}&type=10&dateb=&owner=exclude&count=12`)
+      .then(function(resp) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response;
+      })
+
+    htmlObj.$('tr').filter(function(x) { 
+      return $(this)
+             .children()
+             .eq(0)[0]
+             .innerText == '10-Q'
+    })
+    .map(function(x) {
+      var a = [];
+      a.push($(this).children().eq(1).children().eq(0)[0].href)
+      return a
+    })
+  }, 
+  
+  filterDomFor10KURL: function(htmlObj) {
+    return htmlObj.$('tr').filter(function(x) { 
+      return $(this)
+             .children()
+             .eq(0)[0]
+             .innerText == '10-K'
+    })
+    .map(function(x) {
+      var a = [];
+      a.push($(this).children().eq(1).children().eq(0)[0].href)
+      return a
+    })
+  },
+  
+  searchDomForFinalURL: function(htmlObj) {
+    return htmlObj.$('tr').eq(1).children().eq(2)[0].innerText
+  }
+}
+
+module.exports = apiHelpers;
--- src/server/routes/basic.js
+++ src/server/routes/basic.js
@@ -1,3 +1,21 @@
 /**
  * Placeholder document for all routes that require more than a single function call.e
  */
+
+const apiHelper = require('../helpers/diffAPI.js');
+const 
+
+/** GET TICKER */
+
+const getDoc =async function(ticker, docType = '10-Q') {
+  // get CIK for ticker
+  // var cik = await getCIK(ticker);
+  // get document page urls
+  // var pageURLS = docType === "10-K" ? await apiHelper.filterDomFor10KURL(cik) : await apiHelper.filterDomFor10QURL(cik);
+  // get final urls
+  // var finalURLS = pageURLS.map(async (acc, pageURL) => {
+  //   return await apiHelper.searchDomForFinalURL(pageURL)
+  // })
+  // process diffs
+  // return diffs
+}
