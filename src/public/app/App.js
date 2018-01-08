6b852f6e4de8675cf9a25ae5a6324eeaa54205a2
Anthony Greenheck <aemgreenheck@gmail.com>
2018-01-07 21:22:22 -0500

    [added] message component and error for incorrect ticker input

--- src/public/app/App.js
+++ src/public/app/App.js
@@ -8,12 +8,15 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      symbolToBeSearched: null,
+      symbolToBeSearched: '',
       searchedSymbol: null,
       links: null,
+      message: null,
     };
     this.handleInputSymbol = this.handleInputSymbol.bind(this);
     this.handleSymbolSubmit = this.handleSymbolSubmit.bind(this);
+    this.handleMessage = this.handleMessage.bind(this);
+
   }
   handleInputSymbol(e) {
     this.setState({
@@ -31,8 +34,21 @@ class App extends React.Component {
           searchedSymbol: this.state.symbolToBeSearched,
         })
       })
-      .catch(err => console.error('error returned from fetch!', err))
+      .catch(err => {
+        console.error('error returned from fetch!', err)
+        this.handleMessage('Incorrect ticker, please try again.')
+      })
   }
+  handleMessage(message) {
+		if(!this.state.message) {
+      this.setState({ message })
+      window.setTimeout(() => {
+        this.setState({
+          message: null
+        })
+      }, 3000)
+    }
+	}
   render() {
     return (
       <HashRouter>
@@ -46,6 +62,7 @@ class App extends React.Component {
                 links={this.state.links}
                 symbolToBeSearched={this.state.symbolToBeSearched}
                 searchedSymbol={this.state.searchedSymbol}
+                message={this.state.message}
               />
             )}
           /> 
